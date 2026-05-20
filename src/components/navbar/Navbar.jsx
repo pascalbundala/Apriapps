a6952ba4cee7ef91a58e1a363569b7e2178c0155
Pascal <pascalbundala@gmail.com>
2026-05-20 11:38:38 +0300

    Changing logo name visibility when scrolling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,37 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from "react-router-dom";
 import './navbar.css';
-import { Plus,Minus } from 'lucide-react';
-
+import { Plus, Minus } from 'lucide-react';
 
 const Navbar = () => {
-   const [isOpen, setIsOpen] = useState(false);
 
-    useEffect(() => {
-      if (isOpen) {
-        document.documentElement.classList.add("no-scroll");
+  const [isOpen, setIsOpen] = useState(false);
+  const [hideName, setHideName] = useState(false);
+  const [border, setBorder] = useState(false);
+
+  useEffect(() => {
+
+    // disable scroll when mobile menu is open
+    if (isOpen) {
+      document.documentElement.classList.add("no-scroll");
+    } else {
+      document.documentElement.classList.remove("no-scroll");
+    }
+
+    // detect scrolling
+    const handleScroll = () => {
+
+      if (window.scrollY > 10) {
+        setHideName(true);
+        setBorder(true);
       } else {
-        document.documentElement.classList.remove("no-scroll");
+        setHideName(false);
+        setBorder(false);
       }
 
-      return () => {
-        document.documentElement.classList.remove("no-scroll");
-      };
-    }, [isOpen]);
+    };
 
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      document.documentElement.classList.remove("no-scroll");
+      window.removeEventListener("scroll", handleScroll);
+    };
+
+  }, [isOpen]);
 
   return (
     <div className='navbar'>
 
-      <NavLink to="/" className="logo">
+      <NavLink to="/" className={`logo ${border ? "border" : ""}`}>
+
         <img
           src="/src/assets/icon-white-transparent.png"
           alt="logo for Apriapps"
           className="img-logo"
         />
-        <div className="name-app">
+
+        {/* hide when scrolling */}
+        <div className={`name-app ${hideName ? "hide-name" : ""}`}>
           <h2>apriapps</h2>
         </div>
+
       </NavLink>
 
       <div className="header-desktop">
@@ -39,66 +63,101 @@ const Navbar = () => {
           <NavLink to="/about" className="item-menu">
             About
           </NavLink>
+
           <NavLink to="/project" className="item-menu">
             Project
           </NavLink>
+
           <NavLink to="/contact" className="item-menu">
             Contact
           </NavLink>
         </ul>
-
       </div>
 
-
       <div className="header-mobile">
-        <div className="menu-wrapper"> 
+
+        <div className="menu-wrapper">
+
           {isOpen ? (
-            <div className='close-icon' onClick={() => setIsOpen(false)}>
+            <div
+              className='close-icon'
+              onClick={() => setIsOpen(false)}
+            >
               Close <Minus className='close-icon-icon' />
             </div>
+
           ) : (
-            <div className="cta-menu">             
-              <div className='close-icon' onClick={() => setIsOpen(true)}>
+
+            <div className="cta-menu">
+
+              <div
+                className='close-icon'
+                onClick={() => setIsOpen(true)}
+              >
                 Menu <Plus className='close-icon-icon' />
               </div>
+
             </div>
+
           )}
+
         </div>
 
         <div className={isOpen ? "openMenu" : "closedMenu"}>
-            <div className="nav-open">
-                  <h1>
-                    Let work<br/> together.
-                  </h1>
-                
-                  <div className="links">
-                      <NavLink to="/about" className="title-bold item-menu " onClick={() => setIsOpen(false)}>
-                        About
-                      </NavLink>
-                      <NavLink to="/project" className="title-bold item-menu " onClick={() => setIsOpen(false)}>
-                        Project
-                      </NavLink>
-                      <NavLink to="/contact" className="title-bold item-menu" onClick={() => setIsOpen(false)}>
-                        Contact
-                      </NavLink>
-                  </div>
+
+          <div className="nav-open">
+
+            <h1>
+              Let work<br /> together.
+            </h1>
+
+            <div className="links">
+
+              <NavLink
+                to="/about"
+                className="title-bold item-menu"
+                onClick={() => setIsOpen(false)}
+              >
+                About
+              </NavLink>
+
+              <NavLink
+                to="/project"
+                className="title-bold item-menu"
+                onClick={() => setIsOpen(false)}
+              >
+                Project
+              </NavLink>
+
+              <NavLink
+                to="/contact"
+                className="title-bold item-menu"
+                onClick={() => setIsOpen(false)}
+              >
+                Contact
+              </NavLink>
+
             </div>
 
-            <div className="simple-contact">
-              <h3 className='small-title'>hello@apriapps.com</h3>
-              <h3 className='small-title'>+255(0)785492267</h3>
-              <h3 className='small-title'>2519 new st,Moshi Kilimanjaro TZ</h3>
-            </div>
+          </div>
 
-            <div className="social-links">
-              <a className='small-title' href="">linkedln</a>
-              <a className='small-title' href="">facebook</a>
-              <a className='small-title' href="">twitter</a>
-              <a  className='small-title' href="">dribble</a>
-            </div>
+          <div className="simple-contact">
+            <h3 className='small-title'>hello@apriapps.com</h3>
+            <h3 className='small-title'>+255(0)785492267</h3>
+            <h3 className='small-title'>2519 new st,Moshi Kilimanjaro TZ</h3>
+          </div>
 
-            <div className="background-shadow"></div>
+          <div className="social-links">
+            <a className='small-title' href="">linkedln</a>
+            <a className='small-title' href="">facebook</a>
+            <a className='small-title' href="">twitter</a>
+            <a className='small-title' href="">dribble</a>
+          </div>
+
+          <div className="background-shadow"></div>
+
         </div>
+
       </div>
 
     </div>
